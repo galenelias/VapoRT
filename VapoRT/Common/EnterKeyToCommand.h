f72ca592a522469f9e535b0e37b16e4a4037b56d
galenelias <gelias@gmail.com>
2012-11-18 23:01:43 -0800

    Namespace style tweaks

--- VapoRT/Common/EnterKeyToCommand.h
+++ VapoRT/Common/EnterKeyToCommand.h
@@ -2,33 +2,33 @@
 
 namespace VapoRT
 {
-	public ref class EnterKeyToCommand sealed : public Windows::UI::Xaml::DependencyObject
+	public ref class EnterKeyToCommand sealed : public ::Windows::UI::Xaml::DependencyObject
 	{
 	private:
-		static Windows::UI::Xaml::Interop::TypeName commandType;
-		static Windows::UI::Xaml::Interop::TypeName ownerType;
-		static Windows::UI::Xaml::PropertyMetadata^ commandPropertyMetadata;
-		static Windows::UI::Xaml::DependencyProperty^ _EnterKeyCommandProperty;
+		static ::Windows::UI::Xaml::Interop::TypeName commandType;
+		static ::Windows::UI::Xaml::Interop::TypeName ownerType;
+		static ::Windows::UI::Xaml::PropertyMetadata^ commandPropertyMetadata;
+		static ::Windows::UI::Xaml::DependencyProperty^ _EnterKeyCommandProperty;
 
 	public:
-		static property Windows::UI::Xaml::DependencyProperty^ EnterKeyCommandProperty
+		static property ::Windows::UI::Xaml::DependencyProperty^ EnterKeyCommandProperty
 		{
-			Windows::UI::Xaml::DependencyProperty^ get()
+			::Windows::UI::Xaml::DependencyProperty^ get()
 			{
 				return _EnterKeyCommandProperty;
 			}
 		}
 
-		static Windows::UI::Xaml::Input::ICommand^ EnterKeyToCommand::GetEnterKeyCommand(Windows::UI::Xaml::UIElement^ element)
+		static ::Windows::UI::Xaml::Input::ICommand^ GetEnterKeyCommand(Windows::UI::Xaml::UIElement^ element)
 		{
-			return (Windows::UI::Xaml::Input::ICommand^)element->GetValue(_EnterKeyCommandProperty);
+			return (::Windows::UI::Xaml::Input::ICommand^)element->GetValue(_EnterKeyCommandProperty);
 		};
-		static void EnterKeyToCommand::SetEnterKeyCommand(Windows::UI::Xaml::UIElement^ element, Windows::UI::Xaml::Input::ICommand^ value)
+		static void SetEnterKeyCommand(::Windows::UI::Xaml::UIElement^ element, ::Windows::UI::Xaml::Input::ICommand^ value)
 		{
 			element->SetValue(_EnterKeyCommandProperty, value);
 		};
-		static void RegisterForKeyDown(Windows::UI::Xaml::DependencyObject^ d, Windows::UI::Xaml::DependencyPropertyChangedEventArgs^ e);
-		static void KeyDownEvent(Platform::Object^ sender, Windows::UI::Xaml::Input::KeyRoutedEventArgs ^ e);
+		static void RegisterForKeyDown(::Windows::UI::Xaml::DependencyObject^ obj, ::Windows::UI::Xaml::DependencyPropertyChangedEventArgs^ e);
+		static void KeyDownEvent(::Platform::Object^ sender, ::Windows::UI::Xaml::Input::KeyRoutedEventArgs ^ e);
 
 	};
 }
